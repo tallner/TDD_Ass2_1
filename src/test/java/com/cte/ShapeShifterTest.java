58b5cee2a8748fd251a048c9edcb24ed07d839ee
tallner <ctallner@hotmail.com>
2022-12-05 09:42:10 +0100

    Initial commit

---
+++ src/test/java/com/cte/ShapeShifterTest.java
@@ -0,0 +1,13 @@
+package com.cte;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ShapeShifterTest{
+    @Test
+    public void checkIfListIsEmpty(){
+
+        assertEquals("expected","expected");
+
+    }
+}
